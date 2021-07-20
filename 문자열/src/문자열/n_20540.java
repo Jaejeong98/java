b14499319fa55c91cabb4837a0d9ee85c486fc5d
Jaejeong98 <jaejeong98@naver.com>
2021-07-20 20:18:32 +0900

    2021.07.20

---
+++ 문자열/src/문자열/n_20540.java
@@ -0,0 +1,14 @@
+package ¹®ÀÚ¿­;
+
+import java.util.*;
+
+public class n_20540 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String st[]=sc.nextLine().split("");
+		System.out.print(st[0].equals("E")?"I":"E");
+		System.out.print(st[1].equals("S")?"N":"S");
+		System.out.print(st[2].equals("T")?"F":"T");
+		System.out.print(st[3].equals("J")?"P":"J");
+	}
+}
